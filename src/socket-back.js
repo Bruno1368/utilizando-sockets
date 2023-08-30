d361a3ada7bf861aa6a681239056b5b60cd1ddb8
Bruno <bruno.luis.999@hotmail.com>
2023-08-30 15:38:33 -0300

    Primeiro commit

---
+++ src/socket-back.js
@@ -0,0 +1,53 @@
+import io from "./servidor.js";
+
+const  documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de JavaScript..."
+    },
+    {
+        nome: "Node",
+        texto: "texto de Node..."
+    },
+    {
+        nome: "Socket.io",
+        texto: "texto de Socket.io..."
+    }
+]
+
+io.on("connection", (socket) => {
+    console.log("Um cliente se conectou! ID:", socket.id);
+    
+    socket.on("selecionar_documento", (nomeDocumento) => {
+        socket.join(nomeDocumento)
+
+        const documento = encontrarDocumento(nomeDocumento);
+
+
+        if(documento){
+            socket.emit("texto_documento", documento.texto)
+        }
+     
+
+    })
+
+    socket.on("texto_editor", ({texto, nomeDocumento}) => {
+        const documento = encontrarDocumento(nomeDocumento);
+
+        if(documento) {
+            documento.texto = texto;
+            socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
+        }
+
+    })
+
+})
+
+function encontrarDocumento(nome){
+    const documento = documentos.find((documento) =>  {
+        return documento.nome === nome;
+    })
+
+    return documento
+}
+
