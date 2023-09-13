101bc919f9ca192eb92c6df91ece40a0c2126be4
Bruno <bruno.luis.999@hotmail.com>
2023-09-13 18:56:22 -0300

    teste

--- servidor/registraEventos/cadastro.js
+++ servidor/registraEventos/cadastro.js
@@ -3,7 +3,7 @@ import { cadastrarUsuario, encontrarUsuario } from "../db/usuariosDb.js";
 function registrarEventosCadastro(socket, io) {
     socket.on("cadastrar_usuario", async (dados) => {
         const usuario = await encontrarUsuario(dados.nome)
-
+//
         if(usuario === null) {
             const resultado = await cadastrarUsuario(dados);
             if(resultado.acknowledged) {
